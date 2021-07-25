9b58fccf0bf4d452f48aaa3dc314a391e711ac39
Andrius <andriuss0316@gmail.com>
2021-07-25 21:22:16 +0300

    20210725

---
+++ main.js
@@ -0,0 +1,9 @@
+// penkiolikta uzduotis
+
+function generuotiSocials(selector, iconsList) {
+    // your code goes here
+}
+
+const icons = ['facebook', 'instagram', 'twitter', 'linkedin'];
+
+generuotiSocials('.socials', icons);
